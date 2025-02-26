7cffb8aeb50d41e1d894655a74fdc0867e572d4e
ryuk-d-wild <aiwarlog@gmail.com>
2025-02-26 22:38:39 +0530

    @#$??!%

--- app/(rex)/events/[id]/update/page.tsx
+++ app/(rex)/events/[id]/update/page.tsx
@@ -4,15 +4,14 @@ import { auth } from "@clerk/nextjs/server";
 
 type UpdateEventProps = {
   params: {
-    id: string
-  }
-}
+    id: string;
+  };
+};
 
 const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const authData = await auth();
-
   const userId = authData?.sessionClaims?.userId as string;
-  const event = await getEventById(id)
+  const event = await getEventById(id);
 
   return (
     <>
@@ -21,15 +20,15 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm 
-          type="Update" 
-          event={event} 
-          eventId={event._id} 
-          userId={userId} 
+        <EventForm
+          type="Update"
+          event={event}
+          eventId={event._id}
+          userId={userId}
         />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default UpdateEvent
+export default UpdateEvent;
